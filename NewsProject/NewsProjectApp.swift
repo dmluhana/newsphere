f8c8b7243501703b810a829c4fec29583794de3c
Dilan Luhana <dmluhana@gmail.com>
2023-04-23 15:50:52 -0400

    Initial Commit

---
+++ NewsProject/NewsProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewsProjectApp.swift
+//  NewsProject
+//
+//  Created by Dilan Luhana on 4/23/23.
+//
+
+import SwiftUI
+
+@main
+struct NewsProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
